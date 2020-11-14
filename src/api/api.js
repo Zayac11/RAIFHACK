5a9629e9f891300d960f1936ab64538645e8a096
Zayac11 <podolskiy.2001@mail.ru>
2020-11-14 11:49:42 +0300

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import * as axios from "axios";
+
+let instance = axios.create({
+    // withCredentials: true,
+    baseURL: 'http://127.0.0.1:8000/',
+    // headers: {
+    //     "API-KEY": '3c506123-7591-4f5d-a26a-91140af2545a'
+    // }
+})
+
+export const arenasApi = {
+
+}
+
+export const stadiumApi = {
+
+}
